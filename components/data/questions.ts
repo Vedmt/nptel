7c4634542653556122d98d0a592e94d1d14849ba
Vedmt <vedthigale@gmail.com>
2025-04-26 18:17:31 +0530

    Update questions.ts

--- components/data/questions.ts
+++ components/data/questions.ts
@@ -613,4 +613,611 @@ export const questionsByWeek: { [week: string]: Question[] } = {
 			answer: "Near sources such as lead smelters, mines, and heavily trafficked roadways",
 		},
 	],
+
+	week7: [
+		{
+			question: "What are the immediate health effects of exposure to indoor air pollutants?",
+			options: [
+				"Respiratory diseases and cancer",
+				"Irritation of eyes, nose, and throat",
+				"Cardiovascular diseases",
+				"Neurological",
+			],
+			answer: "Irritation of eyes, nose, and throat",
+		},
+		{
+			question: "Asbestosis, a serious progressive, long-term, non-cancer disease of the lungs is associated with the exposure of:",
+			options: [
+				"Particulate matter",
+				"Second hand smoke",
+				"Asbestos",
+				"Volatile organics compounds (VOCs)",
+			],
+			answer: "Asbestos",
+		},
+		{
+			question: "What is the primary goal of exposure assessment for indoor air pollutants?",
+			options: [
+				"To enhance building aesthetics",
+				"To identify pollutant sources and quantify risks",
+				"To promote the use of natural ventilation",
+				"To calculate construction costs",
+			],
+			answer: "To identify pollutant sources and quantify risks",
+		},
+		{
+			question: "Which type of sampling relies on the kinetic energy of gas molecules and diffusion of the gases in an enclosed space onto a sorbent medium?",
+			options: [
+				"Active sampling",
+				"Passive sampling",
+				"Stratified sampling",
+				"Cluster sampling",
+			],
+			answer: "Passive sampling",
+		},
+		{
+			question: "Which method is commonly used in Computational Fluid Dynamics (CFD) modelling?",
+			options: [
+				"Finite element method",
+				"Box modelling",
+				"Well-mixed model",
+				"Stratified sampling",
+			],
+			answer: "Finite element method",
+		},
+		{
+			question: "Which modelling approach assumes spatially uniform pollutant concentrations indoors?",
+			options: [
+				"Box model",
+				"Well-mixed model",
+				"CFD model",
+				"Advection model",
+			],
+			answer: "Well-mixed model",
+		},
+		{
+			question: "What is the primary advantage of using HEPA filters for air cleaning?",
+			options: [
+				"They remove both gaseous and particulate pollutants.",
+				"They are cost-effective for large spaces.",
+				"They effectively capture fine and ultrafine particles.",
+				"They eliminate microbial contaminants.",
+			],
+			answer: "They effectively capture fine and ultrafine particles.",
+		},
+		{
+			question: "Which technology is most effective in reducing microbial contamination in indoor air?",
+			options: [
+				"UVGI systems",
+				"Ionizers",
+				"Activated carbon filters",
+				"Cyclone separators",
+			],
+			answer: "UVGI systems",
+		},
+		{
+			question: "According to case study \"Personal Exposure to Fine Particles: A Case Study\" which method is used to measure personal exposure to PM2.5?",
+			options: [
+				"High-volume air sampling",
+				"Palm sized optical sensors",
+				"Passive diffusion tubes",
+				"Infrared imaging",
+			],
+			answer: "Palm sized optical sensors",
+		},
+		{
+			question: "According to case study \"Personal Exposure to Fine Particles: A Case Study\" which activities had the highest exposure to fine particles?",
+			options: [
+				"Traffic and cooking",
+				"Market",
+				"Workplace",
+				"Classroom",
+			],
+			answer: "Traffic and cooking",
+		},
+	],
+	week8: [
+		{
+			question: "According to case study \"Indoor Air Quality in Nursery Buildings, UAE – Case Study\" What measures were adopted to monitor indoor air quality in nursery buildings in the UAE?",
+			options: [
+				"Controlling the source of pollutants",
+				"Improving ventilation",
+				"Removing pollutants via purification",
+				"All the above",
+			],
+			answer: "All the above",
+		},
+		{
+			question: "Controlling the source of pollutants, improving ventilation and removing pollutants via purification.",
+			options: [
+				"True",
+				"False",
+			],
+			answer: "True",
+		},
+		{
+			question: "What is the primary substance responsible for ozone layer depletion?",
+			options: [
+				"Methane (CH4)",
+				"Chlorofluorocarbons (CFCs)",
+				"Carbon Dioxide (CO2)",
+				"Ammonia (NH3)",
+			],
+			answer: "Chlorofluorocarbons (CFCs)",
+		},
+		{
+			question: "Which international treaty was established to address ozone depletion?",
+			options: [
+				"Kyoto Protocol",
+				"Paris Agreement",
+				"Montreal Protocol",
+				"Basel Convention",
+			],
+			answer: "Montreal Protocol",
+		},
+		{
+			question: "What is a major cause of global warming?",
+			options: [
+				"Particulate matter emission",
+				"Accumulation of greenhouse gases like CO2 and CH4",
+				"Deforestation in tropical regions",
+				"Loss of biodiversity",
+			],
+			answer: "Accumulation of greenhouse gases like CO2 and CH4",
+		},
+		{
+			question: "Which human activity is the largest contributor to methane emissions?",
+			options: [
+				"Industrial manufacturing",
+				"Fossil fuel combustion",
+				"Livestock farming and rice cultivation",
+				"Urban infrastructure development",
+			],
+			answer: "Livestock farming and rice cultivation",
+		},
+		{
+			question: "GHGs act like a blanket wrapped around the Earth, trapping the outgoing longwave (infrared) radiation from the earth surface and thus declining global atmospheric temperature.",
+			options: [
+				"True",
+				"False",
+			],
+			answer: "False",
+		},
+		{
+			question: "Which Sustainable Development Goals (SDG) focuses on action to combat climate change and its impacts?",
+			options: [
+				"SDG – 6",
+				"SDG – 10",
+				"SDG – 13",
+				"SDG – 16",
+			],
+			answer: "SDG – 13",
+		},
+		{
+			question: "Which pollutants are responsible for the formation of acid rain?",
+			options: [
+				"Methane (CH4) and carbon monoxide (CO)",
+				"Sulfur dioxide (SO2) and nitrogen oxides (NOx)",
+				"Volatile organic compounds (VOCs) and ozone (O3)",
+				"Ammonia (NH3) and particulate matter (PM2.5)",
+			],
+			answer: "Sulfur dioxide (SO2) and nitrogen oxides (NOx)",
+		},
+		{
+			question: "What is a significant effect of acid rain on aquatic ecosystems?",
+			options: [
+				"Increased water temperatures",
+				"Lowering pH levels, leading to the death of aquatic organisms",
+				"Increased levels of dissolved oxygen in water",
+				"Decreased biodiversity in terrestrial environments",
+			],
+			answer: "Lowering pH levels, leading to the death of aquatic organisms",
+		},
+	],
+	week9: [
+		{
+			question: "Which pollutant is most commonly associated with vehicular emissions?",
+			options: [
+				"Lead",
+				"Nitrogen oxides (NOx)",
+				"Ozone",
+				"Radon",
+			],
+			answer: "Nitrogen oxides (NOx)",
+		},
+		{
+			question: "Which device is fitted in vehicles to control air pollution from the tailpipe emissions:",
+			options: [
+				"Electrostatic precipitator",
+				"Catalytic converter",
+				"Bag filter",
+				"Cyclone separator",
+			],
+			answer: "Catalytic converter",
+		},
+		{
+			question: "What is the primary function of cyclone separators in air pollution control?",
+			options: [
+				"Capture gaseous pollutants",
+				"Remove large particulate matter from emissions",
+				"Neutralize sulfur dioxide emissions",
+				"Increase air circulation rates",
+			],
+			answer: "Remove large particulate matter from emissions",
+		},
+		{
+			question: "What is the primary purpose of baghouse filters in industrial settings?",
+			options: [
+				"Remove gaseous pollutants from emissions",
+				"Capture fine particulate matter",
+				"Reduce nitrogen oxides (NOx) levels",
+				"Increase the velocity of exhaust gases",
+			],
+			answer: "Capture fine particulate matter",
+		},
+		{
+			question: "Which air pollution control device is best suited for removing fine mist and liquid droplets?",
+			options: [
+				"Cyclone separator",
+				"Wet scrubber",
+				"Ionizer",
+				"Baghouse filter",
+			],
+			answer: "Wet scrubber",
+		},
+		{
+			question: "Biofiltration can be classified as a special type of absorption system that uses irreversible processes to control gaseous contaminants.",
+			options: [
+				"True",
+				"False",
+			],
+			answer: "True",
+		},
+		{
+			question: "The adsorption process is classified as either physical or chemical. The type of force involved in physical adsorption is",
+			options: [
+				"Chemical interaction",
+				"Van der Waal force",
+				"Centrifugal force",
+				"No force is involved",
+			],
+			answer: "Van der Waal force",
+		},
+		{
+			question: "Which of the following technique is adopted when the contaminant concentration is high enough to make a recovery feasible and economical.",
+			options: [
+				"Biofiltration",
+				"Oxidation",
+				"Condensation",
+				"Absorption",
+			],
+			answer: "Condensation",
+		},
+		{
+			question: "Which technology is used to control odorous emissions from wastewater treatment plants?",
+			options: [
+				"Biofilters",
+				"Cyclones",
+				"Baghouse filters",
+				"Catalytic converters",
+			],
+			answer: "Biofilters",
+		},
+		{
+			question: "Gasoline fumes evaporating from the fuel tank and carburetor are vented directly into the atmosphere. These emissions are significantly reduced by:",
+			options: [
+				"The air-injection system",
+				"Catalytic converter",
+				"Positive crankcase ventilation valve",
+				"Evaporative control system",
+			],
+			answer: "Evaporative control system",
+		},
+	],
+	week10: [
+		{
+			question: "A cyclone has an inlet width of 20 cm and five effective turns (N = 5). The gas temperature is 350⁰K, and the inlet velocity is 10 m/s. The average particle density is 1.5 g/cm³. What is the cut diameter (particle size collected with 50% efficiency)? The viscosity of air at 350⁰K is 0.0748 kg/m-h.",
+			options: [
+				"6.94 μm",
+				"8.33 μm",
+				"12.14 μm",
+				"15.55 μm",
+			],
+			answer: "8.33 μm",
+		},
+		{
+			question: "In a bag house filter, the incoming velocity of the gas is 800 m³/min. the filtering velocity is 4 m/min. Each bag is 20 cm wide and 3 m in length. If the spacing between each bag is 30 m and the wall is 30 cm, the number of bags required is________.",
+			options: [
+				"95",
+				"106",
+				"144",
+				"225",
+			],
+			answer: "106",
+		},
+		{
+			question: "An electrostatic precipitator, in which the collection plate area is 100 m², gas flow rate is 3 m³/s, and collection efficiency is 99.5%. The migration velocity is______(in m/s):",
+			options: [
+				"0.16",
+				"0.67",
+				"1.54",
+				"2.15",
+			],
+			answer: "0.16",
+		},
+		{
+			question: "Single-stage electrostatic precipitator consisting of two sections formed by plates 5.0 m wide and 7.0 m high on 25 cm centers, handling a gas flow of 3 m³/s. Assume that the migration velocity is 12 cm/s. The collection efficiency of a horizontal flow is__________(in %).",
+			options: [
+				"98.55 %",
+				"98.25 %",
+				"99.63 %",
+				"99.25 %",
+			],
+			answer: "99.63 %",
+		},
+		{
+			question: "Gravitational settling chamber under the following conditions: I. Air: Horizontal velocity is 1 m/s Temperature is 75⁰C I. Particle: Specific gravity is 1.5 II. Chamber: Length is 10 m and Height is 2 m III. At 75⁰C viscosity of air is 2.1x10⁻⁵ kg/m-s The minimum size of the particle is____________, that will be removed with 100 per cent efficiency.",
+			options: [
+				"65.28 μm",
+				"71.68 μm",
+				"83.55 μm",
+				"90.67 μm",
+			],
+			answer: "71.68 μm",
+		},
+		{
+			question: "Which of the following pollutants is not included in the revised National Ambient Air Quality Standard (NAAQS, 2009):",
+			options: [
+				"Ozone",
+				"Benzene",
+				"Arsenic",
+				"Cadmium",
+			],
+			answer: "Cadmium",
+		},
+		{
+			question: "What is the annual permissible limit for PM2.5 in ambient air as per WHO guidelines 2021?",
+			options: [
+				"5 μg/m³",
+				"10 μg/m³",
+				"25 μg/m³",
+				"50 μg/m³",
+			],
+			answer: "5 μg/m³",
+		},
+		{
+			question: "What is the primary goal of the National Clean Air Programme (NCAP) in India?",
+			options: [
+				"To completely eliminate PM emissions by 2025",
+				"To achieve a 20–30% reduction in PM2.5 and PM10 levels by 2024",
+				"To reduce vehicular emissions in all cities",
+				"To promote urbanization in non-attainment areas",
+			],
+			answer: "To achieve a 20–30% reduction in PM2.5 and PM10 levels by 2024",
+		},
+		{
+			question: "What is the focus of the Graded Response Action Plan (GRAP)?",
+			options: [
+				"Managing waste in urban areas",
+				"Reducing air pollution during severe episodes in Delhi NCR",
+				"Expanding public transportation networks",
+				"Implementing electric vehicle subsidies",
+			],
+			answer: "Reducing air pollution during severe episodes in Delhi NCR",
+		},
+		{
+			question: "Which Indian policy promotes the adoption of electric vehicles to reduce vehicular emissions?",
+			options: [
+				"National Electric Mobility Mission Plan (NEMMP)",
+				"Make in India Program",
+				"National Renewable Energy Policy",
+				"National Highway Development Project",
+			],
+			answer: "National Electric Mobility Mission Plan (NEMMP)",
+		},
+	],
+	week11: [
+		{
+			question: "In 1991, which of the following protocols was related to the control of emissions of volatile organic compounds (VOCs) or their transboundary fluxes?",
+			options: [
+				"Geneva protocol",
+				"Gothenburg Protocol",
+				"Stockholm Convention",
+				"Helsinki Protocol",
+			],
+			answer: "Geneva protocol",
+		},
+		{
+			question: "What is the primary goal of the Paris Agreement under the UNFCCC?",
+			options: [
+				"To phase out ozone-depleting substances",
+				"To limit global temperature rise to well below 2°C above pre-industrial levels",
+				"To monitor and control air pollution in urban areas",
+				"To eliminate greenhouse gas emissions entirely by 2030",
+			],
+			answer: "To limit global temperature rise to well below 2°C above pre-industrial levels",
+		},
+		{
+			question: "Which treaty focuses on reducing ozone-depleting substances?",
+			options: [
+				"Kyoto Protocol",
+				"Gothenburg Protocol",
+				"Montreal Protocol",
+				"Stockholm Convention",
+			],
+			answer: "Montreal Protocol",
+		},
+		{
+			question: "What was an unintended consequence of improved air quality during lockdowns?",
+			options: [
+				"Decrease in ozone levels in some regions",
+				"Increased indoor air pollution",
+				"Reduced visibility in urban areas",
+				"A slight increase in ground-level ozone in certain cities",
+			],
+			answer: "A slight increase in ground-level ozone in certain cities",
+		},
+		{
+			question: "To control the dust pollution at the construction site, which one of the mitigation measures would be most effective.",
+			options: [
+				"Fogging system",
+				"Ban on refuse burning",
+				"Vehicle usage controls",
+				"Bike parking facilities",
+			],
+			answer: "Fogging system",
+		},
+		{
+			question: "Which of the following mitigation strategies is focused to reduce the emissions from enhanced the use of clean fuel in rural and urban areas?",
+			options: [
+				"Congestion pricing",
+				"Non-motorised transportation",
+				"Pradhan Mantri Ujjwala Yojana",
+				"Auto Fuel Policy",
+			],
+			answer: "Pradhan Mantri Ujjwala Yojana",
+		},
+		{
+			question: "Blending of_______ methanol in gasoline can bring down GHG emissions by 20% in terms of particulate matter, NOx, and SOx, thereby improving the urban air quality.",
+			options: [
+				"10%",
+				"15%",
+				"20%",
+				"25%",
+			],
+			answer: "15%",
+		},
+		{
+			question: "What is a finely divided, carbon-rich, porous substance that can be obtained from the crop stubble through the process of Pyrolysis.",
+			options: [
+				"Composting",
+				"Biochar",
+				"Mechanical processing",
+				"Happy seeder",
+			],
+			answer: "Biochar",
+		},
+		{
+			question: "What is the size range of PM10 particles?",
+			options: [
+				"Greater than 10 microns",
+				"Less than 10 microns in diameter",
+				"Between 10 and 50 microns",
+				"Exactly 10 microns",
+			],
+			answer: "Less than 10 microns in diameter",
+		},
+		{
+			question: "What is the 24 hour National Ambient Air Quality Standard (NAAQS) of PM10 for the residential area (in μg/m³) ?",
+			options: [
+				"40",
+				"60",
+				"80",
+				"100",
+			],
+			answer: "100",
+		},
+	],
+	week12: [
+		{
+			question: "The filter paper used during the sampling of PM2.5 in the ambient air is known as PTFE filter paper, where PTFE refers to",
+			options: [
+				"Poly tetra fluoro ethylamine",
+				"Poly tetra fluoro ethyl",
+				"Poly tetra fluoro ethylene",
+				"Poly tetra fluoro ethanol",
+			],
+			answer: "Poly tetra fluoro ethylene",
+		},
+		{
+			question: "According to National Ambient Air Quality Standards (NAAQS), what is the permissible value of 24 hour-concentration of PM2.5 in Ecologically Sensitive Area (Notified by Central Government)",
+			options: [
+				"40 μg/m³",
+				"60 μg/m³",
+				"80 μg/m³",
+				"100 μg/m³",
+			],
+			answer: "60 μg/m³",
+		},
+		{
+			question: "The absorbing reagent Potassium Tetrachloro mercurate (TCM) is used in sampling and analysis of:",
+			options: [
+				"SO₂",
+				"NO₂",
+				"CO",
+				"O₃",
+			],
+			answer: "SO₂",
+		},
+		{
+			question: "Modified Jacob & Hochheiser Method is used for sampling and analysis of:",
+			options: [
+				"CO",
+				"O₃",
+				"SO₂",
+				"NO₂",
+			],
+			answer: "NO₂",
+		},
+		{
+			question: "In which type of sampling the velocity and the direction of the gas entering the sampling nozzle is the same as that of the gas in the duct/stack at the same sampling point.",
+			options: [
+				"Isothermal sampling",
+				"Isokinetic sampling",
+				"Adiabatic sampling",
+				"Differential sampling",
+			],
+			answer: "Isokinetic sampling",
+		},
+		{
+			question: "Which of the following method is used to measure sulphur dioxide SO2 from the gas sample extracted from the sampling point of the stack.",
+			options: [
+				"Barium thorin titration method",
+				"Improved West and Gaeke method",
+				"Modified Jacob and Hochheiser method",
+				"Gravimetric method",
+			],
+			answer: "Barium thorin titration method",
+		},
+		{
+			question: "Which pollutants are typically monitored using multi-gas monitors?",
+			options: [
+				"CO, CO2, VOCs, and ozone",
+				"PM2.5 and PM10",
+				"Heavy metals and ammonia",
+				"Sulfur dioxide and nitrogen dioxide",
+			],
+			answer: "CO, CO2, VOCs, and ozone",
+		},
+		{
+			question: "Which of the following parameters is measured using the non-dispersive infrared spectroscopy in multi gas monitor.",
+			options: [
+				"Temperature",
+				"Percent Relative Humidity (%RH)",
+				"Carbon Monoxide (CO)",
+				"Carbon Dioxide (CO2)",
+			],
+			answer: "Carbon Dioxide (CO2)",
+		},
+		{
+			question: "What principle is commonly used in spectrometers to analyze particulate matter?",
+			options: [
+				"Light scattering and laser diffraction",
+				"Mass spectrometry",
+				"Gravimetric analysis",
+				"Infrared absorption",
+			],
+			answer: "Light scattering and laser diffraction",
+		},
+		{
+			question: "The minimum time interval for monitoring of the particulate matter using the spectrometer software is:",
+			options: [
+				"45 seconds",
+				"30 seconds",
+				"12 seconds",
+				"6 seconds",
+			],
+			answer: "6 seconds",
+		},
+	],
 };
